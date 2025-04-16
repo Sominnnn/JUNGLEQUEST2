b8093c43243f133ff81b228758b254ec507bc6d9
Khurt Co <cokhurtluis@gmail.com>
2025-04-17 00:24:17 +0800

    TRY DRAG AND DROP

--- app/src/main/java/com/example/junglequest/easy.java
+++ app/src/main/java/com/example/junglequest/easy.java
@@ -1,8 +1,13 @@
 package com.example.junglequest;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,6 +24,31 @@ public class easy extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+        ImageView draggableAnimal = findViewById(R.id.drag_lion);
+
+        draggableAnimal.setOnLongClickListener(v -> {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDragAndDrop(data, shadowBuilder, v, 0); //
+            return true;
+        });
+
+        ImageView dropZone = findViewById(R.id.main);
+
+        dropZone.setOnDragListener((v, event) -> {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    return true;
+                case DragEvent.ACTION_DROP:
+                    View draggedView = (View) event.getLocalState();
+                    draggedView.setX(event.getX());
+                    draggedView.setY(event.getY());
+                    return true;
+                default:
+                    return true;
+            }
         });
     }
 }

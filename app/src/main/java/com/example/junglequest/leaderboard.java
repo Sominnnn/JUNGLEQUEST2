40c156eb48645576ed49964ac82127169cdd74cb
Khurt Co <cokhurtluis@gmail.com>
2025-04-19 15:31:30 +0800

    LEADERBOARD 1ST TRY

--- app/src/main/java/com/example/junglequest/leaderboard.java
+++ app/src/main/java/com/example/junglequest/leaderboard.java
@@ -1,6 +1,9 @@
 package com.example.junglequest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,36 @@ public class leaderboard extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button homeButton = findViewById(R.id.homebtn_leaderboard);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to CardCatalogActivity
+                Intent intent = new Intent(leaderboard.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button instructionsButton = findViewById(R.id.instructionbtn_leaderboard);
+        instructionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to CardCatalogActivity
+                Intent intent = new Intent(leaderboard.this, aboutgame.class);
+                startActivity(intent);
+            }
+        });
+
+        Button playButton = findViewById(R.id.playbtn_leaderboard);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to CardCatalogActivity
+                Intent intent = new Intent(leaderboard.this, choosedif.class);
+                startActivity(intent);
+            }
         });
     }
 }

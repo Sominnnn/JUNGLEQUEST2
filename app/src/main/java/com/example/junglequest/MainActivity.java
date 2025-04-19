c6a812c15ae2f7f776772dc17ad0b0781e9a1678
Khurt Co <cokhurtluis@gmail.com>
2025-04-19 16:29:49 +0800

    HOWTOPLAY BUTTON

--- app/src/main/java/com/example/junglequest/MainActivity.java
+++ app/src/main/java/com/example/junglequest/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Navigate to CardCatalogActivity
-                Intent intent = new Intent(MainActivity.this, aboutgame.class);
+                Intent intent = new Intent(MainActivity.this, howtoplay.class);
                 startActivity(intent);
             }
         });
